310175bb848f766a3bde9ab28da336afe591fc9d
DiSxr <82447033+DiSxr@users.noreply.github.com>
2023-02-28 13:11:26 +0300

    Update Camera.cs
    SecondCommit

--- Assets/Scrips/Camera.cs
+++ Assets/Scrips/Camera.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    [SerializeField] float leftLimit;
+    [SerializeField]
+    float leftLimit;
     [SerializeField]
     float rightLimit;
     [SerializeField]
